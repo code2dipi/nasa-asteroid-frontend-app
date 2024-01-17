ec0e48541631416485455ff9b44d09647c1a9113
code2dipi <63505385+code2dipi@users.noreply.github.com>
2024-01-17 18:09:21 +0100

    Update api.js
    API-KEY

--- src/services/api.js
+++ src/services/api.js
@@ -2,13 +2,14 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:8080/api/asteroids';
+const API_KEY='YOUR-API-KEY' // CHANGE YOUR API KEY HERE
 
 export const getAsteroidsList = (startDate, endDate) => {
-  const url = `${BASE_URL}/landing?startDate=${startDate}&endDate=${endDate}&apiKey=dhYabotopiVNsXMDMyLkGLE3EjomwBi8jMVipGzr`;
+  const url = `${BASE_URL}/landing?startDate=${startDate}&endDate=${endDate}&apiKey=${API_KEY}`;
   return axios.get(url);
 };
 
 export const getAsteroidDetails = (asteroidId) => {
-  const url = `${BASE_URL}/details/${asteroidId}?apiKey=dhYabotopiVNsXMDMyLkGLE3EjomwBi8jMVipGzr`;
+  const url = `${BASE_URL}/details/${asteroidId}?apiKey=${API_KEY}`;
   return axios.get(url);
 };
